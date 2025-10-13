ecadc3e6655ce88702ff05aeb76ece824b232d2e
ducmngx <43735502+ducmngx@users.noreply.github.com>
2025-10-13 20:55:53 +0000

    Deploying to gh-pages from @ ducmngx/ducmngx.github.io@eb7f974844a4f32dc5bf01e12ea38284c5c13268 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "ducmngx/ducmnguyen.github.io",
+    "data-repo": "ducmngx/ducmngx.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
