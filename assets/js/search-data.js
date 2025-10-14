6835355ef22dac3c94f8f6b0b0a2c039b2525a19
ducmngx <43735502+ducmngx@users.noreply.github.com>
2025-10-14 04:39:49 +0000

    Deploying to gh-pages from @ ducmngx/ducmngx.github.io@52626f8c5f658862271338b4c10e9b27e302b555 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -479,6 +479,13 @@ ninja.data = [{
           window.open("https://twitter.com/ducmngx", "_blank");
         },
       },{
+        id: 'social-youtube',
+        title: 'YouTube',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://youtube.com/@off-policy", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
