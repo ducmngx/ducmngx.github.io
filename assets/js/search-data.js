b8414486be2e02dea12db79ded92089c5ac22eb9
ducmngx <43735502+ducmngx@users.noreply.github.com>
2025-10-14 05:13:13 +0000

    Deploying to gh-pages from @ ducmngx/ducmngx.github.io@136777263a08c6223e3733a92dd67446d5cffa71 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,52 +412,7 @@ ninja.data = [{
             },},{id: "news-i-started-working-on-reinforcement-learning-for-robot-navigation-and-locomotion-at-johns-hopkins-institute-for-assured-autonomy-under-dr-tinoosh-mohsenin",
           title: 'I started working on Reinforcement Learning for robot navigation and locomotion at Johns...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-multi-modal-social-human-navigation-dataset-musohu",
-          title: 'Multi-Modal Social Human Navigation Dataset (MuSoHu)',
-          description: "",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{id: "projects-multi-modal-social-human-navigation-dataset-musohu",
+          section: "News",},{id: "projects-multi-modal-social-human-navigation-dataset-musohu",
           title: 'Multi-Modal Social Human Navigation Dataset (MuSoHu)',
           description: "",
           section: "Projects",handler: () => {
